7f2fd290270e9b7c46647dadbfa672233fa39dee
Dang Hoang Ha <ha.dh290702@gmail.com>
2025-05-01 13:32:01 +0300

    Collaborative margin ruler

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -22,10 +22,15 @@ import { FontSize } from '@/extensions/font-size';
 import { LineHeight } from '@/extensions/line-height';
 import { Ruler } from './ruler';
 import { Threads } from './threads';
+import { useStorage } from '@liveblocks/react';
 
 export const Editor = () => {
         const liveblocks = useLiveblocksExtension();
         const { setEditor } = useEditorStore((state) => state);
+
+        const leftMargin = useStorage((root) => root.leftMargin);
+        const rightMargin = useStorage((root) => root.rightMargin);
+
         const editor = useEditor({
                 immediatelyRender: false,
                 onCreate: ({ editor }) => {
@@ -55,7 +60,7 @@ export const Editor = () => {
 
                 editorProps: {
                         attributes: {
-                                style: 'padding-left: 56px; padding-right: 56px',
+                                style: `padding-left: ${leftMargin ?? 56}px; padding-right: ${rightMargin ?? 56}px;`,
                                 class: 'focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1000px] w-[816px] pt-10 pr-14 pb-10 cursor-text',
                         },
                 },
